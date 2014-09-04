1510a994ab42ed121c2962fa3f73c0d8608cd669
Fernando Serapio <fhernandosc@gmail.com>
2014-09-04 15:56:46 -0500

    display contacts and new contacts in a table

--- app/assets/javascripts/views.js
+++ app/assets/javascripts/views.js
@@ -4,11 +4,22 @@ App.Views.App = Backbone.View.extend({
   initialize: function(){
     var addContactView = new App.Views.AddContact({ collection: App.contacts });
 
+    var allContactsView = new App.Views.Contacts({ collection: App.contacts }).render();
+
+    $('#allContacts').append(allContactsView.el);
   }
 });
 
 App.Views.AddContact = Backbone.View.extend({
   el: '#addContact',
+
+  initialize: function(){
+    this.first_name = $('#first_name');
+    this.last_name = $('#last_name');
+    this.email_address = $('#email_address');
+    this.description = $('#description');
+  },
+
   events: {
     'submit': 'addContact'
   },
@@ -17,12 +28,48 @@ App.Views.AddContact = Backbone.View.extend({
     e.preventDefault();
 
     var newContact = this.collection.create({
-      first_name: this.$('#first_name').val(), // this.$el.find('#first_name')
-      last_name: this.$('#last_name').val(),
-      email_address: this.$('#email_address').val(),
-      description: this.$('#description').val()
+      first_name: this.first_name.val(), // this.$el.find('#first_name')
+      last_name: this.last_name.val(),
+      email_address: this.email_address.val(),
+      description: this.description.val()
     }, { wait: true });
 
-    console.log( newContact );
+    this.clearForm();
+  },
+
+  clearForm: function(){
+    this.first_name.val('');
+    this.last_name.val('');
+    this.email_address.val('');
+    this.description.val('');
+  }
+});
+
+App.Views.Contacts = Backbone.View.extend({
+  tagName: 'tbody',
+
+  initialize: function(){
+    this.collection.on('sync', this.addOne, this);
+  },
+
+  render: function(){
+    this.collection.each( this.addOne, this );
+    return this;
+  },
+
+  addOne: function(contact){
+    var contactView = new App.Views.Contact({ model: contact });
+    this.$el.append(contactView.render().el);
+  }
+});
+
+App.Views.Contact = Backbone.View.extend({
+  tagName: 'tr',
+
+  template: template('allContactsTemplate'),
+
+  render: function(){
+    this.$el.html( this.template( this.model.toJSON() ) );
+    return this;
   }
 });
