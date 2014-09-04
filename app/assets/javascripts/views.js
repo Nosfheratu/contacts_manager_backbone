1b9fc4c51d581d0e3c7ac7fdd229aca13ccadf88
Fernando Serapio <fhernandosc@gmail.com>
2014-09-04 18:04:54 -0500

    edit contacts

--- app/assets/javascripts/views.js
+++ app/assets/javascripts/views.js
@@ -1,15 +1,27 @@
 // Backbone Views
 
+/*
+Global App View
+*/
 App.Views.App = Backbone.View.extend({
   initialize: function(){
+    vent.on('contact:edit', this.editContact, this);
     var addContactView = new App.Views.AddContact({ collection: App.contacts });
 
     var allContactsView = new App.Views.Contacts({ collection: App.contacts }).render();
 
     $('#allContacts').append(allContactsView.el);
+  },
+
+  editContact: function(contact){
+    var editContactView = new App.Views.EditContact({ model: contact });
+    $('#editContact').html(editContactView.el);
   }
 });
 
+/*
+Add Contact View
+*/
 App.Views.AddContact = Backbone.View.extend({
   el: '#addContact',
 
@@ -27,7 +39,7 @@ App.Views.AddContact = Backbone.View.extend({
   addContact: function(e){
     e.preventDefault();
 
-    var newContact = this.collection.create({
+    this.collection.create({
       first_name: this.first_name.val(), // this.$el.find('#first_name')
       last_name: this.last_name.val(),
       email_address: this.email_address.val(),
@@ -45,11 +57,61 @@ App.Views.AddContact = Backbone.View.extend({
   }
 });
 
+/*
+Edit Contact View
+*/
+App.Views.EditContact = Backbone.View.extend({
+  template: template('editContactTemplate'),
+
+  initialize: function(){
+    this.render();
+
+    this.form = this.$('form');
+    this.first_name = this.form.find('#edit_first_name');
+    this.last_name = this.form.find('#edit_last_name');
+    this.email_address = this.form.find('#edit_email_address');
+    this.description = this.form.find('#edit_description');
+  },
+
+  events: {
+    'submit form': 'submit',
+    'click button.cancel': 'cancel'
+  },
+
+  submit: function(e){
+    e.preventDefault();
+
+    this.model.save({
+      first_name: this.first_name.val(),
+      last_name: this.last_name.val(),
+      email_address: this.email_address.val(),
+      description: this.description.val()
+    });
+
+    this.remove();
+  },
+
+  cancel: function(){
+    this.remove();
+  },
+
+  render: function(){
+    var html = this.template( this.model.toJSON() );
+
+    this.$el.html(html);
+
+    return this;
+  }
+});
+
+/*
+Contacts View
+*/
 App.Views.Contacts = Backbone.View.extend({
   tagName: 'tbody',
 
   initialize: function(){
-    this.collection.on('sync', this.addOne, this);
+    this.collection.on('add', this.addOne, this);
   },
 
   render: function(){
@@ -63,6 +125,9 @@ App.Views.Contacts = Backbone.View.extend({
   }
 });
 
+/*
+Contact View
+*/
 App.Views.Contact = Backbone.View.extend({
   tagName: 'tr',
 
@@ -70,10 +135,16 @@ App.Views.Contact = Backbone.View.extend({
 
   initialize: function(){
     this.model.on('destroy', this.unrender, this);
+    this.model.on('change', this.render, this);
   },
 
   events: {
-    'click a.delete': 'deleteContact'
+    'click a.delete': 'deleteContact',
+    'click a.edit': 'editContact'
+  },
+
+  editContact: function(){
+    vent.trigger('contact:edit', this.model);
   },
 
   deleteContact: function(){
