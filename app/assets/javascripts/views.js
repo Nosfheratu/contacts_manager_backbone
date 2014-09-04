66b809621f3e1ad7712c9e06dedb1d8625436345
Fernando Serapio <fhernandosc@gmail.com>
2014-09-04 13:55:15 -0500

    add contact form

--- app/assets/javascripts/views.js
+++ app/assets/javascripts/views.js
@@ -2,6 +2,27 @@
 
 App.Views.App = Backbone.View.extend({
   initialize: function(){
-    console.log( this.collection.toJSON() );
+    var addContactView = new App.Views.AddContact({ collection: App.contacts });
+
+  }
+});
+
+App.Views.AddContact = Backbone.View.extend({
+  el: '#addContact',
+  events: {
+    'submit': 'addContact'
+  },
+
+  addContact: function(e){
+    e.preventDefault();
+
+    var newContact = this.collection.create({
+      first_name: this.$('#first_name').val(), // this.$el.find('#first_name')
+      last_name: this.$('#last_name').val(),
+      email_address: this.$('#email_address').val(),
+      description: this.$('#description').val()
+    }, { wait: true });
+
+    console.log( newContact );
   }
 });
